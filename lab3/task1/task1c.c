835b0a4b4ec92f499f882f074af84fd2cb7df858
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-11-07 00:12:26 +0200

    Passed tests

--- lab3/task1/task1c.c
+++ lab3/task1/task1c.c
@@ -27,6 +27,27 @@ void list_print(node *diff_list, FILE *output) {
     }
 }
 
+void list_print_k(node *diff_list, FILE *output, int k) {
+    node *curr = diff_list;
+    diff *data;
+    while (curr != NULL && k > 0) {
+        data = curr->diff_data;
+        fprintf(output, "byte %ld %X %X\n", data->offset, data->orig_value, data->new_value);
+        curr = curr->next;
+        k--;
+    }
+}
+
+int get_list_length(node *diff_list) {
+    node *curr = diff_list;
+    int counter = 0;
+    while (curr != NULL) {
+        curr = curr->next;
+        counter++;
+    }
+    return counter;
+}
+
 /* Add a new node with the given data to the list,
    and return a pointer to the list (i.e., the first node in the list).
    If the list is null - create a new entry and return a pointer to the entry.*/
@@ -59,45 +80,38 @@ unsigned char *read_file(FILE *fp, size_t *result) {
     return buffer;
 }
 
-void task1c(FILE *first_input, FILE *second_input, FILE *output_file, char t_flag, int k) {
+struct node *diff_list(FILE *first_input, FILE *second_input) {
     size_t result1, result2; /*successfully read characters*/
     unsigned char *buffer1 = read_file(first_input, &result1); /* on heap*/
     unsigned char *buffer2 = read_file(second_input, &result2); /* on heap*/
-    int t_counter = 0;
-
     size_t length = (result1 < result2) ? result1 : result2; /* minimum of the two*/
     struct node *list = NULL;
     long i;
     unsigned char c1, c2;
     diff *data = NULL;
-    for (i = 0; i < length && k != 0; i++) {
+    for (i = 0; i < length; i++) {
         c1 = buffer1[i]; /*character in first file*/
         c2 = buffer2[i]; /*character in second file*/
         if (c1 != c2) {
-            if (!t_flag) {
-                data = malloc(sizeof(diff)); /*released at list_free call*/
-                data->orig_value = c1;
-                data->new_value = c2;
-                data->offset = i;
-                list = list_append(list, data);
-            } else t_counter++;
-            if (k != -1)k--;
+            data = malloc(sizeof(diff)); /*released at list_free call*/
+            data->orig_value = c1;
+            data->new_value = c2;
+            data->offset = i;
+            list = list_append(list, data);
         }
     }
-    if (!t_flag) {
-        list_print(list, output_file);
-        list_free(list);
-    } else fprintf(output_file, "%d\n", t_counter);
     if (buffer1) free(buffer1);
     if (buffer2) free(buffer2);
+    return list;
 }
+
 
 int main(int argc, char **argv) {
     FILE *first_input = NULL;
     FILE *second_input = NULL;
     FILE *output_file = stdout;
     char t_flag = 0, input_flag = 0; /* flags */
-    int k = -1,i;
+    int k = -1, i;
     for (i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-k") == 0) k = atoi(argv[++i]);
         else if (strcmp(argv[i], "-t") == 0) t_flag = 1;
@@ -105,9 +119,14 @@ int main(int argc, char **argv) {
         else if (!input_flag) {
             first_input = fopen(argv[i], "rb"); /*read first input file*/
             input_flag = 1;
-        } else second_input = fopen(argv[i], "rb"); /*read second input file*/
+        } else second_input = fopen(argv[i], "rb+"); /*read second input file*/
     }
-    task1c(first_input, second_input, output_file,t_flag,k);
+    struct node *list = diff_list(first_input, second_input);
+    if (t_flag) fprintf(output_file, "%d\n", get_list_length(list));
+    else if (k > 0)list_print_k(list, output_file, k);
+    else list_print(list, output_file);
+    /* cleaning */
+    list_free(list);
     if (first_input)fclose(first_input);
     if (second_input)fclose(second_input);
     if (output_file != stdout)fclose(output_file);
