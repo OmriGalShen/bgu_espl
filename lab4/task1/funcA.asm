e4e434709de3d20340e33e847a9c7466e3c197d7
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-11-12 23:22:58 +0200

    partially answered task1

--- lab4/task1/funcA.asm
+++ lab4/task1/funcA.asm
@@ -2,19 +2,19 @@ section .text
 	global funcA
 	
 funcA:
-	push	ebp
-	push	ebx
+	push	ebp; save for after function finished
+	push	ebx; save for after function finished
 	mov	ebp, esp
-	mov	eax,-1
+	mov	eax,-1; initale value for eax with -1
 
-.L2:
-	add eax, 1
-	mov ebx, eax
-	add ebx, [ebp+12]
-	movzx	ebx, BYTE [ebx]
-	test bl,bl
-	jne .L2
+.L2: ;
+	add eax, 1 ;eax := i = 0,1,2,..
+	mov ebx, eax; ebx = i
+	add ebx, [ebp+12]; ebx = i + parameter:=a 
+	movzx	ebx, BYTE [ebx]; ebx = bl + zeroes (first byte of i+a) --  exb = ebx%256
+	test bl,bl ;bit-wise AND of bl (without affecting bl)
+	jne .L2; loop until bl=0
 	mov esp, ebp
-	pop ebx
-	pop ebp
-	ret
+	pop ebx; restore previous ebx
+	pop ebp; restore previous ebp
+	ret ; finished with eax = i s.t first byte of a + i = 0
