383cbd6cf16ee8f7c50560319ab2ea170f42f678
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-11-12 23:23:53 +0200

    Completed task3

--- lab4/task3/task3.s
+++ lab4/task3/task3.s
@@ -7,6 +7,7 @@ section .rodata
     string2 db 'BBA', 0
     string3 db 'BB', 0
     string4 db '', 0
+    string5 db 'SSAASSS', 0
 
 section .text
     global _start
@@ -14,12 +15,30 @@ section .text
 
 _start:
     sub  esp, 4 ; allocate stack space for printf result
-    push 'A' ; argument 3 to printf
-    push prefix ; argument 2 to printf
-    push format ; argument 1 to printf
-    call printf
-    add esp, 12 ; return the stack pointer to it's original location
+    mov eax, 0; char counter
+    mov ebx, STR; current char
 
+count_loop:
+    cmp [ebx], BYTE 0; end of string
+    je print_res; print result
+    cmp [ebx], BYTE X; current char is x
+    je found_char
+    inc ebx; next char
+    jmp count_loop
+
+found_char:
+    inc eax; eax++
+    inc ebx; next char
+    jmp count_loop
+
+print_res:
+    push eax ; argument 4 to printf
+    push STR ; argument 3 to printf
+    push X ; argument 2 to printf
+    push print_format ; argument 1 to printf
+    call printf
+    add esp, 16 ; return the stack pointer to it's original location
+    
 exit:
     mov ebx, 0
     mov eax, 1
