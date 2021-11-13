0a2168ddb313c703975233a22de092e08e55421d
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-11-13 02:05:53 +0200

    Added files to tesk1 accroding to insturctions

---
+++ lab4/task1/task1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+extern int funcA(char* str);
+
+int main(int argc, char **argv)
+{
+    if(argc != 2) 
+        printf("Please enter a string\n");
+    else
+        printf("%d\n",funcA(argv[1]));
+    return 0;
+}
