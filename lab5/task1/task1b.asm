f6874dc352b596dca951174e758152809c9de15b
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-11-27 20:30:24 +0200

    Working on task1c

--- lab5/task1/task1b.asm
+++ lab5/task1/task1b.asm
@@ -1,7 +1,11 @@
-%define UTOA_BUFLEN 16
+%define UTOA_BUFLEN 16 ; for utoa_s
 
 section .bss
-str: RESB UTOA_BUFLEN
+str: RESB UTOA_BUFLEN ; for utoa_s
+
+section .data
+length: dd 0; for atou_s
+count: dd 0; for atou_s
 
 section .text
 	global utoa_s
@@ -32,7 +36,7 @@ nextDigit:
 	mov edx, 0; clear edx
 	mov ebx, 10
 	div ebx; eax = i/10, dl = i%10
-	add edx, '0'
+	add edx, '0' ; convert from number to ASCII
 	mov [ecx], dl
 	mov ebx, eax; ebx = i/10
 	jmp whileNotZero
@@ -52,8 +56,44 @@ atou_s:
     push ebp ;stack maintenance
 	push ebx
 	mov ebp, esp ;stack maintenance
-    mov eax, dword [ebp+12] ;char* c
-    jmp FINISH
+    mov ebx, dword [ebp+12] ;char* c
+	mov edx, 0 
+	mov ecx, ebx
+
+get_string_length:
+	cmp [ecx], BYTE 0
+	je string_loop
+	inc dword [length]
+	inc ecx
+	jmp get_string_length
+
+string_loop:
+	cmp [ebx], BYTE 0
+	je result
+	movzx eax, BYTE [ebx]; current char*
+	sub eax, '0'; convert ascii to int
+
+init_mul_loop:
+	mov ecx, dword [length]
+	mov [count], ecx
+	mov ecx, 10
+
+mul_loop:; eax = eax*10*(length-1)
+	cmp [count], dword 1
+	je done_mul
+	mul ecx; eax = 10*eax
+	dec dword [count]
+	jmp mul_loop
+
+done_mul:
+	add edx, eax
+	dec dword [length]
+	inc ebx
+	jmp string_loop
+
+result:
+	mov eax, edx
+	jmp FINISH
 
 FINISH:
 	mov esp, ebp
