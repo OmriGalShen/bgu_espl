a7bf1ad56e972b20bc3503d4245a4e68cf0d920c
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-12-04 13:56:15 +0200

    Finished task0

--- lab6/task0/task0.c
+++ lab6/task0/task0.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include "LineParser.h"
 
 #define MAX_READ 1<<11
 
-int main()
-{
-    long size;
-    char *abs_path_name;
-    size = pathconf(".", _PC_PATH_MAX);
-    char buf[size];
-    abs_path_name = getcwd(buf, (size_t)size);
+void execute(cmdLine *pCmdLine);
 
-    while(1){
-    printf("%s> ",abs_path_name);
-    char user_input[MAX_READ];
-    fgets(user_input, MAX_READ,stdin);
-    
-    if(strcmp(user_input,"quit")==0)
-        break;
-    cmdLine* command = parseCmdLines(user_input);
-    
-    printf("%s\n",command->arguments[0]);
+char *getAbsPath();
+
+int main() {
+    char *abs_path_name = getAbsPath();
+
+    while (1) {
+        printf("%s> ", abs_path_name); //prints path
+
+        char user_input[MAX_READ];
+        fgets(user_input, MAX_READ, stdin);
+        user_input[strcspn(user_input, "\n")] = 0; // Removing trailing newline
+
+        if (strcmp(user_input, "quit") == 0) // exit condition
+            break;
+
+        cmdLine *command = parseCmdLines(user_input);
+        execute(command);
+        freeCmdLines(command);
     }
-    
     return 0;
+}
+
+char *getAbsPath() {
+    long max_path_len;
+    max_path_len = pathconf(".", _PC_PATH_MAX);
+    char buff[max_path_len];
+    return getcwd(buff, (size_t) max_path_len);
+}
+
+void execute(cmdLine *pCmdLine) {
+//    int res = execv(pCmdLine->arguments[0],pCmdLine->arguments); //example "/bin/ls -l"
+    int res = execvp(pCmdLine->arguments[0], pCmdLine->arguments); //look for commands in your PATH setting
+    if (res == -1)
+        perror("execvp failed");
 }
