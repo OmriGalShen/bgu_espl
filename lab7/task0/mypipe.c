ca97ac6a548f47f151582942ffc18652d43083e2
Omri-Gal-Shenhav <shenhav.omri@gmail.com>
2021-12-10 16:27:10 +0200

    Finished task0

--- lab7/task0/mypipe.c
+++ lab7/task0/mypipe.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv) {
-    printf("hello world\n");
-    return 0;
+int main() {
+    int file_des[2];
+    pid_t child_pid;
+    char string[] = "Hello!\n";
+
+
+    pipe(file_des);
+
+    if ((child_pid = fork()) == -1) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (child_pid == 0) {
+        close(file_des[0]);//Child process does not need this end of the pipe
+
+        /* Send "string" through the output side of pipe */
+        write(file_des[1], string, (strlen(string) + 1));
+        exit(0);
+    } else {
+        /* Parent process closes up output side of pipe */
+        close(file_des[1]);//Parent process does not need this end of the pipe
+
+        char read_buffer[1<<7];
+        /* Read in a string from the pipe */
+        read(file_des[0], read_buffer, sizeof(read_buffer));
+        printf("Child message: %s", read_buffer);
+    }
+
+    return (0);
 }
-
