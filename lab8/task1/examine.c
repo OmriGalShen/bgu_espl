f782c7cf5352d6ffb13d943d66678226c10d4ddb
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-12-17 03:04:39 +0200

    Refactored task1

--- lab8/task1/examine.c
+++ lab8/task1/examine.c
@@ -28,23 +28,24 @@ int main(int argc, char *argv[])
 
     if (size < 5 || !(header->e_ident[0] == 0x7f && header->e_ident[1] == 'E' && header->e_ident[2] == 'L' && header->e_ident[3] == 'F'))
     {
-        fprintf(stderr, "open invalid elf file format");
+        fprintf(stderr, "Invalid elf file format\n");
         return 1;
     }
 
-    Elf32_Shdr *shdr = (Elf32_Shdr *)(map_start + header->e_shoff);
-    int shnum = header->e_shnum;
+    Elf32_Shdr *section_header = (Elf32_Shdr *)(map_start + header->e_shoff);
+    int num_of_sections = header->e_shnum;
+    Elf32_Shdr *sh_strtab = &section_header[header->e_shstrndx];
+    const char *const sh_strtab_p = map_start + (sh_strtab)->sh_offset;
 
-    Elf32_Shdr *sh_strtab = &shdr[header->e_shstrndx];
-    const char *const sh_strtab_p = map_start + sh_strtab->sh_offset;
-    Elf32_Shdr *curr_section;
     printf("[Nr] Name                 Addr     Off      Size\n");
-    for (int i = 0; i < shnum; ++i)
+    for (int i = 0; i < num_of_sections; ++i)
     {
-        curr_section = (Elf32_Shdr *)(((char *)header) + header->e_shoff + i * header->e_shentsize);
-        const char *section_name = sh_strtab_p + shdr[i].sh_name;
+        const char *section_name = sh_strtab_p + section_header[i].sh_name;
+        Elf32_Addr addr = section_header[i].sh_addr;
+        Elf32_Off offset = section_header[i].sh_offset;
+        Elf32_Word size = section_header[i].sh_size;
         printf("[%-2d] %-20s %08x %08x %08x\n", i,
-               section_name, curr_section->sh_addr, curr_section->sh_offset, curr_section->sh_size);
+               section_name, addr, offset, size);
     }
 
     close(fd);
