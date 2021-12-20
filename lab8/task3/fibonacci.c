090a89134dd2c0a6814a632b07ded24c72899581
Omri Gal Shenhav <shenhav.omri@gmail.com>
2021-12-20 16:05:14 +0200

    Fixed according to marina

--- lab8/task3/fibonacci.c
+++ lab8/task3/fibonacci.c
@@ -8,25 +8,46 @@ void printer(int num)
 
 int fib(int limit)
 {
-    int a = 0, b = 1, temp = -1;
-    char flag = 0;
-    for (int i = 0; i < limit; i++)
-    {
-        flag = 1;
+    int a = 0, b = 1, temp = 1, last_a = -1, i;
+
+    for (i = 0; i < limit; i++) {
         printer(a);
-        temp = a;
+        last_a = a;
+        temp += a;
         a = b;
-        b += temp;
+        b = temp;
     }
-    return flag ? a : -1;
+
+    // just some garbage that does nothing to waste space here
+    for (i = 0; i < limit; i++);
+
+    return last_a;
+}
+
+void goodbye(int limit, int last) {
+
+    for (int i = 0; i < 100; i++)
+        printf("%s", "");
+
+    char s[] = "Fvsflb";
+
+    for (int i = 0; i < 6; i++)
+        s[i]--;
+
+    printf("\n\n%s!\n\n", last < limit ? s : "Asta la vista baby");
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        printf("Usage: fib <limit>");
+    int res;
+
+    if (argc < 2) {
+        printf("Usage: fib <limit>\n");
+        exit(1);
     }
-    fib(atoi(argv[1]));
-    printf("\n");
+
+    res = fib(atoi(argv[1]));
+    goodbye(atoi(argv[1]), res);
+
+    return 0;
 }
--- lab8/task3/task3.c
+++ lab8/task3/task3.c
@@ -8,23 +8,43 @@ void printer(int num)
 
 int fib(int limit)
 {
-    int a = 0, b = 1, temp = -1;
-    while (a <= limit)
-    {
+    int a = 0, b = 1, temp = 1, last_a = -1, i;
+
+    while (a<=limit) {
         printer(a);
-        temp = a;
+        last_a = a;
+        temp += a;
         a = b;
-        b += temp;
+        b = temp;
     }
-    return temp;
+
+    return last_a;
+}
+
+void goodbye(int limit, int last) {
+
+    for (int i = 0; i < 100; i++)
+        printf("%s", "");
+
+    char s[] = "Fvsflb";
+
+    for (int i = 0; i < 6; i++)
+        s[i]--;
+
+    printf("\n\n%s!\n\n", last < limit ? s : "Asta la vista baby");
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        printf("Usage: fib <limit>");
+    int res;
+
+    if (argc < 2) {
+        printf("Usage: fib <limit>\n");
+        exit(1);
     }
-    fib(atoi(argv[1]));
-    printf("\n");
+
+    res = fib(atoi(argv[1]));
+    goodbye(atoi(argv[1]), res);
+
+    return 0;
 }
